1e22d3b71cf5aff46a4af6f0ef1484211426d370
Isargenys Contreras <48723570+Isargenys@users.noreply.github.com>
2022-08-23 22:24:34 -0400

    vista caja

---
+++ caja.php
@@ -0,0 +1,156 @@
+<?php
+include "config/connection.php";
+
+if(!isset($_SESSION['username']) && !empty($_SESSION['username'])){    
+    header('Location: login.php');
+}
+
+if(isset($_POST['btn_logout'])){
+    session_destroy();
+    header('Location: login.php');
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmacy System - Categorias</title>
+    <?php include ("headerLinks.php"); ?>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/caja.css">
+
+    
+</head>
+<body>
+<div class="d-flex" id="wrapper">
+    <?php include('sidenav.php') ?>
+    <div id="page-content-wrapper">
+        <?php include ("navbar.php"); ?>
+        <div class="container-fluid">
+            <h1 class="text-center fw-bold mb-3 mt-3">
+                Caja
+            </h1>
+            <hr>
+            <h4 class="ms-3 mb-5 nuevaVenta fw-normal">Nueva venta</h4>
+            <div class="container  shadow-sm">
+                <div class="row">
+                    <div class="col-3">
+                <div class="mb-3">
+                <label for="fechaVenta" class="form-label">Fecha de venta</label>
+                <input type="date" class="form-control" id="fechaVenta">
+                </div>
+                    </div>
+                    <div class="col-6">
+                    <div class="mb-3">
+                <label for="cliente" class="form-label">Cliente</label>
+                <input type="text" class="form-control" id="cliente">
+                </div>
+                    </div>
+                    <div class="col-3">
+                    <div class="mb-3">
+                <label for="telefono" class="form-label">Telefono</label>
+                <input type="text" class="form-control" id="telefono">
+                </div>
+                    </div>
+                </div>
+                <div class="row">
+               <div class="col-4">
+               <label for="buscarProducto" class="form-label">Buscar producto</label>
+                <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Buscar producto" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary border-0 btn-buscar" type="button" id="buscarProducto">
+                <i class="bi bi-search"></i>
+                </button>
+               </div>
+                </div>
+                <div class="col-3">
+                <div class="mb-3">
+                <label for="precio" class="form-label">Precio</label>
+                <input type="text" class="form-control" id="precio">
+                </div>
+                </div>
+                <div class="col-3">
+                <div class="mb-3">
+                <label for="cantidad" class="form-label">Cant.</label>
+                <input type="text" class="form-control" id="cantidad">
+                </div>
+                 </div>
+                 <div class="col-2">
+                <div class="mb-3">
+                <button type="button" class=" btn-agregar border-0 p-2 rounded-3">
+                    Agregar
+                </button>
+                </div>
+                 </div>
+                 <div class="col-3">
+                <div class="mb-3">
+                <label for="Vendedor" class="form-label">Vendedor</label>
+                <input type="text" class="form-control" id="vendedor">
+                </div>
+                 </div>
+                </div>
+                
+                    <div class="d-flex mt-4 pb-4">
+                <button type="button" class=" btn-facturar border-0 p-2 rounded-3 mx-4">
+                Facturar
+                </button>
+                <button type="button" class=" btn-borrar border-0 p-2 rounded-3 ">
+                    Borrar factura
+                </button>
+                    </div>
+            </div>
+            <div class="container">
+            <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Código</th>
+            <th scope="col">Cant.</th>
+            <th scope="col">Descripción</th>
+            <th scope="col">Precio unit.</th>
+            <th scope="col">Descuento</th>
+            <th scope="col">Precio total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">112</th>
+            <td>5</td>
+            <td>Dramidom</td>
+            <td>5.00</td>
+            <td>5%</td>
+            <td>25.00</td>
+            <td>
+            <i class="bi bi-x-circle"></i>
+            </td>
+            </tr>
+            <tr>
+            <th scope="row">232</th>
+            <td>3</td>
+            <td>Fendramin</td>
+            <td>10</td>
+            <td>10%</td>
+            <td>100</td>
+            <td>
+            <i class="bi bi-x-circle"></i>
+            </td>
+            </tr>
+        </tbody>
+        </table>
+            </div>
+        
+        </div>
+    </div>
+</div>   
+
+
+
+<?php include ("footerLinks.php"); ?>
+
+</body>
+</html>
--- sidenav.php
+++ sidenav.php
@@ -6,35 +6,34 @@
          <i class="bi bi-heart-pulse-fill text-white-50"></i>
         </div>
     <div class="list-custom list-group list-group-flush">
-        <a class="list-group-item list-group-item-action list-group-item-light p-3 " href="dashboard.php">
+        <a class="list-group-item   p-3 " href="dashboard.php">
         <i class="bi bi-house-door fs-5"></i>  
         Inicio
         </a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" data-bs-toggle="collapse" href="#collapseInventario" role="button" aria-expanded="false" aria-controls="collapseExample">
+        <a class="list-group-item   p-3" data-bs-toggle="collapse" href="#collapseInventario" role="button" aria-expanded="false" aria-controls="collapseExample">
         <i class="bi bi-card-list fs-5"></i>
         Inventario <i class="bi bi-chevron-down float-end"></i>
         </a>
         <div class="collapse" id="collapseInventario">
             <div class="list-group">
-            <a href="categorias.php" class="list-group-item list-group-item-action">Categorias</a>
-            <a href="productos.php" class="list-group-item list-group-item-action">Productos</a>
-            <a href="" class="list-group-item list-group-item-action">Proveedores</a>
+            <a href="categorias.php" class="list-group-item  submenu">Categorias</a>
+            <a href="productos.php" class="list-group-item  submenu ">Productos</a>
             </div>
         </div>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" data-bs-toggle="collapse" href="#collapseTienda">
+        <a class="list-group-item   p-3" data-bs-toggle="collapse" href="#collapseTienda">
         <i class="bi bi-shop fs-5"></i>
             Tienda <i class="bi bi-chevron-down float-end"></i></a>
         <div class="collapse" id="collapseTienda">
             <div class="list-group">
-            <a href="" class="list-group-item list-group-item-action">Ventas</a>
-            <a href="puntoVenta.php" class="list-group-item list-group-item-action">Punto de Ventas</a>
+            <a href="caja.php" class="list-group-item  submenu">Caja</a>
+            <a href="puntoVenta.php" class="list-group-item  submenu">Ventas</a>
             </div>
         </div>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">
+        <a class="list-group-item list-group-item-action  p-3" href="#!">
         <i class="bi bi-clipboard-data fs-5"></i>
             Reportes
         </a>
-        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">
+        <a class="list-group-item   p-3" href="#!">
         <i class="bi bi-people fs-5"></i>
         Empleados
         </a>
