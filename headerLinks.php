19c7e920ed7f3f628f2d3439514b10ad56b44185
Isargenys Contreras <48723570+Isargenys@users.noreply.github.com>
2022-08-27 12:18:28 -0400

    Bootstrap
    Carpeta de estilos bootstrap

--- headerLinks.php
+++ headerLinks.php
@@ -1,5 +1,4 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-<!-- <link rel="stylesheet" href="https://bootswatch.com/5/minty/bootstrap.min.css"> -->
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet" href="asset/css/style.css">
+<link rel="stylesheet" href="asset/css/custom.css">
