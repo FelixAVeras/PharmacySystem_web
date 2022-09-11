5647112b35cd4769fa9694bc3b278fd41e34f266
Isargenys Contreras <48723570+Isargenys@users.noreply.github.com>
2022-09-11 18:02:10 -0400

    formulario agregar empleado

--- addEmployee.php
+++ addEmployee.php
@@ -20,26 +20,99 @@ include ("headerLinks.php");
         <?php include('sidenav.php') ?>
         <div id="page-content-wrapper">
             <?php include ("navbar.php"); ?>
+
             <div class="container-fluid">
             
-                <h3 class=" mt-3">Nuevo Empleado</h3>
+            <h4 class=" mt-5 ms-5 nuevoEmpleado">Nuevo Empleado</h4>
+                <div class="container mt-5 d-flex justify-content-center shadow-sm w-75 rounded-3">
+                
 
                 <form action="">
                     <div class="row mb-3">
                         <div class="col-12 col-md-4">
                             <div class="form-group">
-                                <label for="" class="control-label"></label>
+                                <label for="" class="control-label">
+                                Numero de empleado
+                                </label>
                                 <input type="text" class="form-control">
                             </div>
                         </div>
                         <div class="col-12 col-md-4">
-                            <div class="form-group"><label for="" class="control-label"></label><input type="text" class="form-control"></div>
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                Nombre
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
                         </div>
                         <div class="col-12 col-md-4">
-                            <div class="form-group"><label for="" class="control-label"></label><input type="text" class="form-control"></div>
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                    Apellido
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                Telefono
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                Dirección
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                    Correo
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                Permisos
+                                </label>
+                                <select class="form-select" aria-label="Default select example">
+                                <option selected>Administrador</option>
+                                <option value="1">Cajero</option>
+                                <option value="2">Vendedor</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                Usuario
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="" class="control-label">
+                                    Clave
+                                </label>
+                                <input type="text" class="form-control">
+                            </div>
                         </div>
                     </div>
                 </form>
+                </div>
+
 
             </div>
 
